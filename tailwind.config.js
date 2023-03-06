7f13d44911ea62a4bbb2337bc13c77d3add947c6
angeldev07 <angelgaran20@gmail.com>
2023-03-05 22:32:16 -0500

    Creado el componente y estilos del login

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'darkBg' : '#132840'
+      }
+    },
   },
   plugins: [],
 }
